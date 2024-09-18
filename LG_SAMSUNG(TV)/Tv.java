2997e37f92ddd64b82d7d4960d590a7abf6f96b8
0taeho0 <166297377+0taeho0@users.noreply.github.com>
2024-09-18 19:15:13 +0900

    Create Tv.java

---
+++ LG_SAMSUNG(TV)/Tv.java
@@ -0,0 +1,9 @@
+package _0918_resetTv_Lg_and_Samsung;
+
+public interface Tv {
+    boolean Tv_on_and_off();
+    int change_channel();
+    void check_channel();
+    boolean pairing_setting();
+    void tv_name_print();
+}
