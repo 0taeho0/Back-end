a0e2abd70ba184cf767636e7b532909effef65e6
0taeho0 <166297377+0taeho0@users.noreply.github.com>
2024-10-15 17:26:17 +0900

    Create SamsungTv.java

---
+++ teachercode/Tv/SamsungTv.java
@@ -0,0 +1,77 @@
+package teachercode;
+
+public class SamsungTv implements Tv {
+    // 변수 부분
+    private int currentChannel = 1;
+    private boolean powerOn = false;
+    private boolean paired= false;
+
+    // 메서드 부분
+    @Override
+    public void setPower(boolean power) {
+        // 리모컨과 페어링된 경우에만 전원을 켜거나 끌 수 있음.
+        if(paired) {
+            powerOn = power;
+            if(power) {
+                System.out.println("Samsung Tv is now ON.");
+            } else {
+                System.out.println("samsung Tv is now OFF.");
+            }
+        }else{
+            System.out.println("Samsung Tv is not paired.");
+        }
+    }
+
+    @Override
+    public void changeChannel(int channel) {
+        // 리모컨과 페어링이되고, Tv의 전원이 켜져 있는 상태에만 체널 변경 가능
+        if(paired && powerOn){
+            this.currentChannel= channel;
+            System.out.println("Samsung Tv Channel to channel " + channel);
+        } else if (!paired) {
+            System.out.println("Samsung Tv is not paired");
+        }else{
+            System.out.println("Samsung Tv is OFF. Cannot change channel.");
+        }
+    }
+
+    @Override
+    public int getCurrentChannel() {
+        return currentChannel;
+    }
+
+    @Override
+    public boolean isPowerOn() {
+        return powerOn;
+    }
+
+    @Override
+    public boolean isPaired() {
+        return paired;
+    }
+
+    @Override
+    public void setPair(boolean pair) {
+        paired = pair;
+        if(pair) {
+            System.out.println("Samsung Tv paired with remote.");
+        }else {
+            System.out.println("Samsung Tv unpaired from remote.");
+        }
+    }
+
+    @Override
+    public String getTvName() {
+        return "Samsung Tv";
+    }
+
+    // 고유 가능(메서드) Samsung : NETFLIX 스트리밍 가능
+    public void watchNetflix(){
+        // Tv의 전원이 켜져 있을 때만 넷플릭스 시청할 수 있음.
+        if (powerOn) {
+            System.out.println("Samsung Tv : Now streaming Netflix");
+        }else {
+            System.out.println("Samsung Tv is OFF. Cannot stream Netflix.");
+        }
+    }
+}
